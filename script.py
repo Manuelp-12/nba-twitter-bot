1c73f49d4176a9876dbe86e495026ad27f46760f
Manuel Paul <139383972+Manuelp-12@users.noreply.github.com>
2025-08-05 12:38:28 -0400

    Add files via upload

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import tweepy
 import csv
 import random
 import time
+from tweepy.errors import TooManyRequests
 from credentials import *
 
 players = [] #array of all players
@@ -17,8 +18,18 @@ client = tweepy.Client(consumer_key=api_key, consumer_secret=api_secret, access_
 
 while (len(players) > 0):
     random_player = random.choice(players) #chooses random player
-    players.remove(random_player) #removes random player
 
-    client.create_tweet(text=random_player[0]) #converts array element to string
-    print("Player printed and removed.")
-    time.sleep(30 * 60) #runs every 30 mins
+    try:
+        client.create_tweet(text=random_player[0]) #converts array element to string
+        print("Player printed and removed.")
+        players.remove(random_player) #removes random player
+
+        #rewrite csv file with new list
+        with open('nbaplayerlist.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(players)
+
+        time.sleep(85 * 60) #runs every 85 mins (17 tweets a day)
+    except TooManyRequests as e:
+        print("Hit rate limit.")
+        time.sleep(10 * 60) # sleep for 10 min
